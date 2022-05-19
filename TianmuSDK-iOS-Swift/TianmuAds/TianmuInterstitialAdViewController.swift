286758a85d5c802261b47511fe59a1228757579d
liujuan <>
2022-05-19 11:01:02 +0800

    update demo

--- TianmuSDK-iOS-Swift/TianmuAds/TianmuInterstitialAdViewController.swift
+++ TianmuSDK-iOS-Swift/TianmuAds/TianmuInterstitialAdViewController.swift
@@ -11,6 +11,7 @@ class TianmuInterstitialAdViewController: BaseViewController, TianmuInterstitial
 
     var interstitialAd: TianmuInterstitialAd?
     var isReady:Bool = false
+    var isNormalAd:Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,29 +23,62 @@ class TianmuInterstitialAdViewController: BaseViewController, TianmuInterstitial
         loadBtn.layer.cornerRadius = 3;
         loadBtn.clipsToBounds = true;
         loadBtn.backgroundColor = UIColor.white
-        loadBtn.setTitle("加载激励视频", for: .normal)
+        loadBtn.setTitle("加载普通插屏", for: .normal)
         loadBtn.setTitleColor(UIColor.black, for: .normal)
         self.view.addSubview(loadBtn)
-        loadBtn.frame = CGRect.init(x: 30, y: UIScreen.main.bounds.size.height/2-60, width: UIScreen.main.bounds.size.width-60, height: 40)
+        loadBtn.frame = CGRect.init(x: 30, y: 100, width: UIScreen.main.bounds.size.width-60, height: 40)
         loadBtn.addTarget(self, action: #selector(loadInterstitialAd), for: .touchUpInside)
         
         let showBtn = UIButton.init()
         showBtn.layer.cornerRadius = 3
         showBtn.clipsToBounds = true
         showBtn.backgroundColor = UIColor.white
-        showBtn.setTitle("展示激励视频", for: .normal)
+        showBtn.setTitle("展示普通插屏", for: .normal)
         showBtn.setTitleColor(UIColor.black, for: .normal)
         self.view.addSubview(showBtn)
-        showBtn.frame = CGRect.init(x: 30, y: UIScreen.main.bounds.size.height/2+20, width: UIScreen.main.bounds.size.width-60, height: 40)
+        showBtn.frame = CGRect.init(x: 30, y: 160, width: UIScreen.main.bounds.size.width-60, height: 40)
         showBtn.addTarget(self, action: #selector(showInterstitialAd), for: .touchUpInside)
+        
+        let loadBidBtn = UIButton.init()
+        loadBidBtn.layer.cornerRadius = 3
+        loadBidBtn.clipsToBounds = true
+        loadBidBtn.backgroundColor = UIColor.white
+        loadBidBtn.setTitle("加载竞价插屏", for: .normal)
+        loadBidBtn.setTitleColor(UIColor.black, for: .normal)
+        self.view.addSubview(loadBidBtn)
+        loadBidBtn.frame = CGRect.init(x: 30, y: 220, width: UIScreen.main.bounds.size.width-60, height: 40)
+        loadBidBtn.addTarget(self, action: #selector(loadBidAd), for: .touchUpInside)
+        
+        let bidWinBtn = UIButton.init()
+        bidWinBtn.layer.cornerRadius = 3
+        bidWinBtn.clipsToBounds = true
+        bidWinBtn.backgroundColor = UIColor.white
+        bidWinBtn.setTitle("竞价成功", for: .normal)
+        bidWinBtn.setTitleColor(UIColor.black, for: .normal)
+        self.view.addSubview(bidWinBtn)
+        bidWinBtn.frame = CGRect.init(x: 30, y: 280, width: UIScreen.main.bounds.size.width-60, height: 40)
+        bidWinBtn.addTarget(self, action: #selector(bidWin), for: .touchUpInside)
+        
+        let bidFailBtn = UIButton.init()
+        bidFailBtn.layer.cornerRadius = 3
+        bidFailBtn.clipsToBounds = true
+        bidFailBtn.backgroundColor = UIColor.white
+        bidFailBtn.setTitle("竞价失败", for: .normal)
+        bidFailBtn.setTitleColor(UIColor.black, for: .normal)
+        self.view.addSubview(bidFailBtn)
+        bidFailBtn.frame = CGRect.init(x: 30, y: 280, width: UIScreen.main.bounds.size.width-60, height: 40)
+        bidFailBtn.addTarget(self, action: #selector(bidFail), for: .touchUpInside)
+    
+        
     }
     
     @objc func loadInterstitialAd() {
+        isNormalAd = true;
         // 1、初始化插屏广告对象
         interstitialAd = TianmuInterstitialAd.init()
         interstitialAd!.delegate = self
         interstitialAd!.controller = self
-        interstitialAd!.posId = "418f6bdc0a3e"
+        interstitialAd!.posId = "682f5d1cb439"
         // 2、加载插屏广告
         interstitialAd?.loadData()
     }
@@ -57,42 +91,126 @@ class TianmuInterstitialAdViewController: BaseViewController, TianmuInterstitial
         }
     }
     
+    @objc func loadBidAd(){
+        isNormalAd = false
+        // 1、初始化插屏广告对象
+        interstitialAd = TianmuInterstitialAd.init()
+        interstitialAd!.delegate = self
+        interstitialAd!.controller = self
+        interstitialAd!.posId = "03bd2a589fe1"
+        // 2、加载插屏广告
+        interstitialAd?.loadData()
+    }
+    
+    @objc func bidWin(){
+        if isNormalAd{
+            return
+        }
+        let otherPlatPrice : Int = 1
+        interstitialAd?.sendWinNotification(withPrice: otherPlatPrice)
+        if isReady{
+            interstitialAd?.show(fromRootViewController: self)
+            return
+        }
+        self.view.makeToast("广告未准备好")
+    }
+    
+    @objc func bidFail(){
+        if isNormalAd{
+            return
+        }
+        interstitialAd?.sendWinFailNotificationReason(.lowPrice, winnerPirce: 1000)
+        if isReady{
+            interstitialAd?.show(fromRootViewController: self)
+            return
+        }
+        self.view.makeToast("广告未准备好")
+    }
+    /**
+     *  插屏广告数据请求成功
+     */
     func tianmuInterstitialSuccess(toLoad unifiedInterstitial: TianmuInterstitialAd) {
         print(#function)
         // 3、展示插屏广告
+        if !isNormalAd{
+            self.view.makeToast("当前广告价格：+ Int(unifiedInterstitial.bidPrice())")
+        }
         isReady = true
-        self.view.makeToast("插屏广告准备完成")
+        self.view.makeToast("广告准备好")
     }
-    
+    /**
+     *  插屏广告渲染成功
+     */
     func tianmuInterstitialRenderSuccess(_ unifiedInterstitial: TianmuInterstitialAd) {
         
     }
-    
+    /**
+     *  插屏广告数据请求失败
+     */
     func tianmuInterstitialFail(toLoad unifiedInterstitial: TianmuInterstitialAd, error: Error) {
         print(#function)
+        self.view.makeToast("广告加载失败")
         interstitialAd = nil
     }
-    
+    /**
+     *  插屏广告视图展示失败回调
+     *  插屏广告展示失败回调该函数
+     */
     func tianmuInterstitialFail(toPresent unifiedInterstitial: TianmuInterstitialAd, error: Error) {
         print(#function)
         interstitialAd = nil
     }
-    
+    /**
+     *  插屏广告视图展示成功回调
+     *  插屏广告展示成功回调该函数
+     */
     func tianmuInterstitialDidPresentScreen(_ unifiedInterstitial: TianmuInterstitialAd) {
         
     }
-    
+    /**
+     *  插屏广告曝光回调
+     */
     func tianmuInterstitialWillExposure(_ unifiedInterstitial: TianmuInterstitialAd) {
         
     }
-    
+    /**
+     *  插屏广告点击回调
+     */
     func tianmuInterstitialClicked(_ unifiedInterstitial: TianmuInterstitialAd) {
         
     }
     
+    /**
+     *  插屏广告页关闭
+     */
     func tianmuInterstitialAdDidDismissClose(_ unifiedInterstitial: TianmuInterstitialAd) {
         self.interstitialAd = nil
     }
-    
-
+    /**
+     插屏视频广告开始播放
+     */
+    func tianmuInterstitialAdVideoPlay(_ unifiedInterstitial: TianmuInterstitialAd){
+        
+    }
+    /**
+     插屏视频广告视频播放失败
+     */
+    func tianmuInterstitialAdVideoPlayFail(_ unifiedInterstitial: TianmuInterstitialAd, error: Error) {
+        
+    }
+    /**
+     插屏视频广告视频暂停
+     */
+    func tianmuInterstitialAdVideoPause(_ unifiedInterstitial: TianmuInterstitialAd){
+        
+    }
+    /**
+     插屏视频广告视频播放完成
+     */
+    func tianmuInterstitialAdVideoFinish(_ unifiedInterstitial: TianmuInterstitialAd){
+        
+    }
+    func tianmuInterstitialAdDidCloseLandingPage(_ unifiedInterstitial: TianmuInterstitialAd){
+        
+    }
 }
