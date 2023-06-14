c6acfc18c1524fddf5796c82766dd5bc77178a45
liujuan <>
2023-06-14 14:02:54 +0800

    Update Demo.

--- TianmuSDK-iOS-Swift/TianmuAds/TianmuBannderViewController.swift
+++ TianmuSDK-iOS-Swift/TianmuAds/TianmuBannderViewController.swift
@@ -13,11 +13,83 @@ class TianmuBannderViewController: BaseViewController, TianmuBannerAdViewDelegat
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        bannerAdView = TianmuBannerAdView.init(frame: CGRect.init(x: 0, y: 250, width: SCREEN_WIDTH, height: SCREEN_WIDTH / (640/100.0)), posId: "e592f0da3b71")
-        bannerAdView!.delegate = self
-        bannerAdView?.viewController = self
-        self.view.addSubview(bannerAdView!)
+    
+        let margin = self.view.bounds.size.width/5.0 - 320.0/5.0
+        
+        let loadAndShowBtn = UIButton.init()
+        loadAndShowBtn.setTitle("普通请求", for: UIControl.State.normal)
+        loadAndShowBtn.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        loadAndShowBtn.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+        loadAndShowBtn.backgroundColor = UIColor.lightGray
+        loadAndShowBtn.clipsToBounds = true
+        loadAndShowBtn.layer.cornerRadius = 3
+        loadAndShowBtn.addTarget(self, action: #selector(loadNomarlAd), for: UIControl.Event.touchUpInside)
+        loadAndShowBtn.frame = CGRect.init(x: margin, y: self.view.bounds.size.height - 60, width: 80, height: 30)
+        self.view.addSubview(loadAndShowBtn)
+        self.view.bringSubviewToFront(loadAndShowBtn)
+        
+        let loadBtn = UIButton.init()
+        loadBtn.setTitle("竞价请求", for: UIControl.State.normal)
+        loadBtn.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        loadBtn.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+        loadBtn.backgroundColor = UIColor.lightGray
+        loadBtn.clipsToBounds = true
+        loadBtn.layer.cornerRadius = 3
+        loadBtn.addTarget(self, action:#selector(loadBidAd), for: UIControl.Event.touchUpInside)
+        loadBtn.frame = CGRect.init(x: margin*2 + 80, y: self.view.bounds.size.height - 60, width: 80, height: 30)
+        self.view.addSubview(loadBtn)
+        self.view.bringSubviewToFront(loadBtn)
+        
+        let bidWinBtn = UIButton.init()
+        bidWinBtn.setTitle("竞价成功", for: UIControl.State.normal)
+        bidWinBtn.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        bidWinBtn.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+        bidWinBtn.backgroundColor = UIColor.lightGray
+        bidWinBtn.clipsToBounds = true
+        bidWinBtn.layer.cornerRadius = 3
+        bidWinBtn.addTarget(self, action: #selector(bidWin), for: UIControl.Event.touchUpInside)
+        bidWinBtn.frame = CGRect.init(x: 160 + margin*3, y: self.view.bounds.size.height - 60, width: 80, height: 30)
+        self.view.addSubview(bidWinBtn)
+        self.view.bringSubviewToFront(bidWinBtn)
+        
+        let bidFailBtn = UIButton.init()
+        bidFailBtn.setTitle("竞价失败", for: UIControl.State.normal)
+        bidFailBtn.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        bidFailBtn.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+        bidFailBtn.backgroundColor = UIColor.lightGray;
+        bidFailBtn.clipsToBounds = true
+        bidFailBtn.layer.cornerRadius = 3
+        bidFailBtn.addTarget(self, action: #selector(bidFail), for: UIControl.Event.touchUpInside)
+        bidFailBtn.frame = CGRect.init(x: margin*4 + 240, y: self.view.bounds.size.height - 60, width: 80, height: 30)
+        self.view.addSubview(bidFailBtn)
+        self.view.bringSubviewToFront(bidFailBtn)
+    }
+   
+    @objc func loadNomarlAd() {
+        let frame = CGRect(x: 0, y: 250, width: SCREEN_WIDTH, height: SCREEN_WIDTH / (640/100.0))
+        bannerAdView = createBannerAdView(frame: frame, posId: "e592f0da3b71")
         bannerAdView?.loadRequest()
+    }
+
+    @objc func loadBidAd() {
+        let frame = CGRect(x: 0, y: 250, width: SCREEN_WIDTH, height: SCREEN_WIDTH / (640/100.0))
+        bannerAdView = createBannerAdView(frame: frame, posId: "ew454yryrtt5667")
+        bannerAdView?.loadRequest()
+    }
+    
+    @objc func bidWin(){
+        bannerAdView?.sendWinNotification(withPrice: 100)
+    }
+    @objc func bidFail(){
+        bannerAdView?.sendWinFailNotificationReason(TianmuAdBiddingLossReason.other, winnerPirce: 100)
+    }
+    
+    func createBannerAdView(frame: CGRect, posId: String) -> TianmuBannerAdView {
+        let bannerAdView = TianmuBannerAdView(frame: frame, posId: posId)
+        bannerAdView.delegate = self
+        bannerAdView.viewController = self
+        self.view.addSubview(bannerAdView)
+        return bannerAdView
     }
     
     /**
@@ -25,7 +97,7 @@ class TianmuBannderViewController: BaseViewController, TianmuBannerAdViewDelegat
      *  当接收服务器返回的广告数据成功后调用该函数
      */
     func tianmuBannerSuccessLoad(_ tianmuBannerView: TianmuBannerAdView) {
-        
+        // 重要‼️ 如果是竞价广告位，且支持自刷新，需要处理自刷新的竟赢上报
     }
     
     /**
